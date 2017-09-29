368984ad7f2feb32dafe18c950ed3a02080b4ec0
bachiri Taoufiq Abderrahman <bachiri.abderrahman@gmail.com>
2017-09-29 10:21:05 +0100

    java string reverse problem

---
+++ Java/java/Strings/JavaStringReverse.java
@@ -0,0 +1,37 @@
+package ocp.preparation.chapter1;
+
+import java.io.*;
+import java.time.Year;
+import java.util.*;
+
+//https://www.hackerrank.com/challenges/java-string-reverse/problem
+
+public class JavaStringReverse {
+
+    static final String YES = "Yes";
+    static final String NO = "No";
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        System.out.println(isApalidrom(A));
+
+
+    }
+
+    private static String isApalidrom(String a) {
+
+        char[] b = a.toCharArray();
+        String returnVal = YES;
+
+        for (int i = 0; i < a.length() / 2; i++) {
+            if (b[i] != b[(a.length() - 1) - i])
+                returnVal = NO;
+        }
+        return returnVal;
+    }
+
+
+}

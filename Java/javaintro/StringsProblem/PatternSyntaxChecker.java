497f8e7ce9a313e3e721273a6cfdf77ee9b1ea3c
bachiri Taoufiq Abderrahman <bachiri.abderrahman@gmail.com>
2017-11-22 12:32:57 +0000

    Pattern Syntax Checker

--- Java/javaintro/StringsProblem/PatternSyntaxChecker.java
+++ Java/javaintro/StringsProblem/PatternSyntaxChecker.java
@@ -1,15 +1,43 @@
 package javaintro.StringsProblem;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
+
+/**
+ * https://www.hackerrank.com/challenges/pattern-syntax-checker/problem
+ * <p>
+ * Difficulty: Easy
+ * Author : Bachiri Taoufiq Abderrahman
+ */
 public class PatternSyntaxChecker {
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
-        while(testCases>0){
-            String pattern = in.nextLine();
-            //Write your code
+        try (Scanner in = new Scanner(System.in)) {
+
+            int testCases = Integer.parseInt(in.nextLine());
+            List<String> patternStateList = new ArrayList<>();
+
+            for (int i = 0; i < testCases; i++) {
+                String validState = "Valid";
+                String patternString = in.nextLine();
+                try {
+                    Pattern pattern = Pattern.compile(patternString);
+                } catch (PatternSyntaxException e) {
+                    validState = "Invalid";
+                }
+                patternStateList.add(validState);
+            }
+            for (String state : patternStateList) {
+                System.out.println(state);
+
+            }
         }
+
+
     }
 }
+

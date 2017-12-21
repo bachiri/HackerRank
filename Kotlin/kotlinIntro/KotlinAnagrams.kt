9aad1cd9e1c5d1df95b61b8f3ce3858bcbce0fbd
bachiri Taoufiq Abderrahman <bachiri.abderrahman@gmail.com>
2017-12-21 23:09:50 +0000

    Anagrams Kotlin

--- Kotlin/kotlinIntro/KotlinAnagrams.kt
+++ Kotlin/kotlinIntro/KotlinAnagrams.kt
@@ -1,13 +1,15 @@
 package javaintro.StringsProblem
+
 import java.util.Scanner
 import java.util.TreeMap
+
 /**
  * Problem Link :https://www.hackerrank.com/challenges/java-anagrams/problem
  * <p>
  * Difficulty: Easy
  */
 object KotlinAnagrams {
-    fun isAnagram(a:String, b:String):Boolean {
+    fun isAnagram(a: String, b: String): Boolean {
         //Our Algorithm is case Insensitive
         var a = a.toLowerCase()
         var b = b.toLowerCase()
@@ -21,28 +23,31 @@ object KotlinAnagrams {
             isAnagram = true
         return isAnagram
     }
+
     //O(n)
-    fun BuildMap(a:String):Map<Char, Int> {
+    fun BuildMap(a: String): Map<Char, Int> {
         val returnMap = TreeMap<Char, Int>()
-        for (i in 0 until a.length)
-        {
-            val key = a.get(i)
-            if (returnMap.containsKey(key))
-            {
-                val integer = returnMap.get(key)
-                returnMap.put(key, (integer?.toInt()?.plus(1)!!))
-            }
-            else
-                returnMap.put(key, 1)
-        }
+        (0 until a.length)
+                .map { a[it] }
+                .forEach {
+                    if (returnMap.containsKey(it)) {
+                        val integer = returnMap[it]
+                        returnMap.put(it, (integer!!.toInt().plus(1)!!))
+                    } else
+                        returnMap.put(it, 1)
+                }
         return returnMap
     }
-    @JvmStatic fun main(args:Array<String>) {
-        val scan = Scanner(System.`in`)
-        var a = scan.next()
-        var b = scan.next()
-        scan.close()
-        val ret = isAnagram(a, b)
-        println(if ((ret)) "Anagrams" else "Not Anagrams")
-    }
+
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    var a = scan.next()
+    var b = scan.next()
+    scan.close()
+    val ret = KotlinAnagrams.isAnagram(a, b)
+    println(if (ret) "Anagrams" else "Not Anagrams")
+
 }
